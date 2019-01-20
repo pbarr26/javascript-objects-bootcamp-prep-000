c778d751a954dfb95069061eabc768916c40c2b0
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-20 20:48:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,12 +2,3 @@ var playlist = {
   artistName: 'songTitle'
 }
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName]=songTitle
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
-  return playlist
-}
